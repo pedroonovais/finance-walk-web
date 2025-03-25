42f48e55eb62c7360736fa502d80249079957265
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-25 20:43:10 -0300

    refactor navbar

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 export default function CategoriesPage(){
     return (
         <>
-            <NavBar active="categorias"/>
+            <NavBar active="movimentações" />
             <main className="flex justify-center">
                 <div className="bg-slate-900 m-4 p-4 rounded min-w-2/3">
-                    <h2>Categorias</h2>
+                    <h2>Movimentações</h2>
                 </div>
             </main>
         </>
--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,13 +1,30 @@
 import Link from "next/link";
 
-export default function NavBar(){
+interface NavBarProps{
+    active: "dashboard" | "movimentações" | "categorias"
+}
+
+export default function NavBar(props: NavBarProps){
+    const { active } = props
+    const activeClass = "border-b-4 border-pink-600"
+
+    const links = [
+        { label: "dashboard", href: "/dashboard" },
+        { label: "movimentações", href: "/transactions" },
+        { label: "categorias", href: "/categories" },
+    ]
+
     return(
         <nav className="flex justify-between items-center bg-slate-900 p-6">
             <h1 className="text-2xl font-bold">Finance Walk</h1>
             <ul className="flex gap-4">
-                <li><Link href="#">dashboard</Link></li>
-                <li><Link href="#">movimentações</Link></li>
-                <li className="border-b-4 border-pink-600"><Link href="#">categorias</Link></li>
+
+                {links.map( link => 
+                    <li className={active === link.label ? activeClass : ""}>
+                        <Link href={link.href}>{link.label}</Link>
+                    </li>
+                )}
+
             </ul>
             <img className="size-12 rounded-full" src="http://github.com/pedroonovais.png" alt="Foto de perfil" />
         </nav>
